0778be6b3105166e546a47e0f08651be5f0c250d
Evens Antoine <evens7antoine@gmail.com>
2025-02-22 01:55:05 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let first = ['double', 'vanilla', 'mac', 'spicy', 'strawberry'];
+  let second = ['cheese', 'ice', 'and', 'hot', 'milk'];
+  let third = ['burger', 'cream', 'cheese', 'pockets', 'shake'];
+
+  for (let i = 0; i < first.length; i++) {
+    console.log('i=', i)
+    for (let s = 0; s < second.length; s++) {
+      console.log('s=', s)
+      for (let t = 0; t < third.length; t++) {
+        console.log(first[i] + second[s] + third[t]);
+        console.log('t=', t)
+      }
+    }
+  }
+}
